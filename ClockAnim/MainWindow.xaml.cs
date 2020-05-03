c1c1ea59e6bd758e35c53439e9bf50808920368e
BohdanZyshchyk <bohdan.zyshchuk@zagravagames.com>
2020-05-04 01:40:39 +0300

    final commit

--- ClockAnim/MainWindow.xaml.cs
+++ ClockAnim/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +24,17 @@ namespace ClockAnim
         public MainWindow()
         {
             InitializeComponent();
+            Storyboard seconds = (Storyboard)sec.FindResource("sbSec");
+            seconds.Begin();
+            seconds.Seek(new TimeSpan(0, 0, 0, DateTime.Now.Second, 0));
+
+            Storyboard minutes = (Storyboard)min.FindResource("sbMin");
+            minutes.Begin();
+            minutes.Seek(new TimeSpan(0, 0, DateTime.Now.Minute,0, 0));
+
+            Storyboard hours = (Storyboard)hour.FindResource("sbHour");
+            hours.Begin();
+            hours.Seek(new TimeSpan(0, DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second, 0));
         }
     }
 }
